4b80baa51a865c33242808c81dc76cc27feb9926
NishantSyadav <263435187+NishantSyadav@users.noreply.github.com>
2026-09-19 23:51:48 +0530

    Ml CERTFICATE

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ const certificates = [
   name: "Supervised Machine Learning: Regression and Classification",
   issuer: "DeepLearning.AI · Stanford Online",
   date: "2026",
-  image: "certificates/certificateML.PNG",
+  image: "certificates/certificateML.png",
   link: "https://coursera.org/verify/09GF3OHD2Y4D"
 },
   {
